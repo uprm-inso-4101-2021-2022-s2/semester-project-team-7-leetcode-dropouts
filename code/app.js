c74aa54c07d4f0a37d5b99665c4f3003640e428d
Keiven-Soto <keiven.soto@upr.edu>
2022-03-17 18:10:29 -0400

    Pomodoro File completed

--- code/app.js
+++ code/app.js
@@ -1,6 +1,13 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+const countdown = document.querySelector(".countdown");
+const date = new Date(25,00);
+
+console.log(date);
+
+countdown.innerHTML = '<div>${minutes}</div> <div>${seconds}</div>';
+
 menu.addEventListener('click', function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
