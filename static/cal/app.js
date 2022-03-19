3c7cef10df0be10434a00a5697c35512a40c1efa
Eduardo-Pineiro <eduardo.pineiro@upr.edu>
2022-03-18 21:35:43 -0400

    merged html files to backend

--- static/cal/app.js
+++ static/cal/app.js
@@ -1,7 +1,35 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
+const countdown = document.querySelector(".countdown");
+const date = new Date(25,00);
+
+console.log(date);
+
+countdown.innerHTML = '<div>${minutes}</div> <div>${seconds}</div>';
+
 menu.addEventListener('click', function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+const AddTaskBtn = document.querySelector(".task__btn");
+
+AddTaskBtn.addEventListener("click", myFunction());
+
+
+function myFunction() {
+  let input = prompt("Please Enter Task", "");
+  if(input != null){
+    document.getElementById("InputAdded").innerHTML = input;
+    let Date= prompt("Enter Due Date: ");
+    if(Date !=null ){
+      document.getElementById("Date").innerHTML = Date;
+      let rating = prompt("Importance Rating: ");
+      if(rating != null){
+        document.getElementById("Rating").innerHTML = rating;
+      }
+    }
+
+  }
+}
