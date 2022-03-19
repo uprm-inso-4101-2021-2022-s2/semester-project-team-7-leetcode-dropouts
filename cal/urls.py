be6131486901c1cd4c5c6a4383941185c57c0ce9
Eduardo-Pineiro <eduardo.pineiro@upr.edu>
2022-03-18 21:54:00 -0400

    add signin views

--- cal/urls.py
+++ cal/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('tasks/', views.tasks, name = 'tasks'),
     path('pomodoro/', views.pomodoro, name = 'pomodoro'),
+    path('signin/', views.signin, name = 'signin'),
 ]
