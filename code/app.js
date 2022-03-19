f674fba99f2a643064ea85018dbd814e69e2b474
Keiven-Soto <keiven.soto@upr.edu>
2022-03-18 21:20:42 -0400

    Task HTML updated

--- code/app.js
+++ code/app.js
@@ -12,3 +12,24 @@ menu.addEventListener('click', function(){
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+const AddTaskBtn = document.querySelector(".task__btn");
+
+AddTaskBtn.addEventListener("click", myFunction());
+
+
+function myFunction() {
+  let input = prompt("Please Enter Task", "");
+  if(input != null){
+    document.getElementById("InputAdded").innerHTML = input;
+    let Date= prompt("Enter Due Date: ");
+    if(Date !=null ){
+      document.getElementById("Date").innerHTML = Date;
+      let rating = prompt("Importance Rating: ");
+      if(rating != null){
+        document.getElementById("Rating").innerHTML = rating;
+      }
+    }
+
+  }
+}
