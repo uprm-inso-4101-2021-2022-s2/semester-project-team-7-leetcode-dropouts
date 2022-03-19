cd981302b1592b8787f81215fe11be668dbe7850
Eduardo-Pineiro <eduardo.pineiro@upr.edu>
2022-03-18 21:05:13 -0400

    tasks - Account models

--- cal/models.py
+++ cal/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
+    account_id = models.IntegerField(primary_key=True)
     account_name = models.CharField(max_length=128)
     username = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
@@ -9,6 +10,12 @@ class Account(models.Model):
 
     def __str__(self):
         return self.username
+
+class Tasks(models.Model):
+    task_id = models.IntegerField(primary_key=True)
+    task_Title = models.CharField(max_length=128)
+    task_imrating = models.IntegerField()
+    tied_to = models.ForeignKey(Account, on_delete=models.CASCADE)
 
 class Test(models.Model):
 
