3f763a4defb9a89e9b12dfd39042d9f1b76d6d5a
Eduardo-Pineiro <eduardo.pineiro@upr.edu>
2022-03-26 16:02:55 -0400

    add ssl to urls - linux issue

--- cal/urls.py
+++ cal/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 
 from . import views
 
+from django.conf import settings
+
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('tasks/', views.tasks, name = 'tasks'),
     path('pomodoro/', views.pomodoro, name = 'pomodoro'),
     path('signin/', views.signin, name = 'signin'),
     path('signup/', views.signup, name = 'signup'),
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+    {'document_root': settings.STATIC_DOC_ROOT}),
 ]
